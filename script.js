b594d9d3cf1386d23d19ab51f487ffcaa1140fb1
BuildTools <xabhista19@gmail.com>
2023-11-24 19:53:09 +0700

    feat: added more complex logic :D

--- script.js
+++ script.js
@@ -5,44 +5,79 @@ const printOut = (content) => {
 
 const roundRandom = (number) => Math.round(Math.random() * number);
 
-const mathRound1 = Math.round(Math.random() * 10);
-const mathRound2 = Math.round(Math.random() * 90);
-const mathRound3 = Math.round(Math.random() * 20);
-
-const mathFloor1 = Math.floor(Math.random() * 20);
-const mathFloor2 = Math.floor(Math.random() * 30);
-const mathFloor3 = Math.floor(Math.random() * 50);
-
-const mathCeil1 = Math.ceil(Math.random() * 100);
-const mathCeil2 = Math.ceil(Math.random() * 70);
-const mathCeil3 = Math.ceil(Math.random() * 80);
-
-// results can be doubles or decimal, using Math.round is possible to get a round number although the chance is not high
-const mathPow1 = Math.pow(roundRandom(10), 2);
-const mathPow2 = Math.pow(roundRandom(20), 2);
-const mathPow3 = Math.pow(roundRandom(30), 2);
-
-// results can be doubles or decimal, using Math.round is possible to get a round number although the chance is not high
-const mathSqrt1 = Math.sqrt(roundRandom(1000));
-const mathSqrt2 = Math.sqrt(roundRandom(2000));
-const mathSqrt3 = Math.sqrt(roundRandom(3000));
-
+// let it be enum :D
 const mthRnd = "Math Round";
 const mthFlr = "Math Floor";
 const mthCl = "Math Ceil";
 const mthPow = "Math Pow";
 const mthSqrt = "Math Sqrt";
 
-const resultsRnd = [[mthRnd, mathRound1], [mthRnd, mathRound2], [mthRnd, mathRound3]];
-const resultsFlr = [[mthFlr, mathFloor1], [mthFlr, mathFloor2], [mthFlr, mathFloor3]];
-const resultsCl = [[mthCl, mathCeil1], [mthCl, mathCeil2], [mthCl, mathCeil3]];
-const resultsPow = [[mthPow, mathPow1], [mthPow, mathPow2], [mthPow, mathPow3]];
-const resultsSqrt = [[mthSqrt, mathSqrt1], [mthSqrt, mathSqrt2], [mthSqrt, mathSqrt3]];
-const resultList = [resultsRnd, resultsFlr, resultsCl, resultsPow, resultsSqrt];
+// use num 2 if the math function requires 2 numbers
+const doTheMath = (num1, num2, curIndex) => {
+    if(curIndex == 0) {
+        return Math.round(num1);
+    }
 
-// a better way to print out the results in a clean way
-resultList.forEach((result,) => {
-    result.forEach((item, index) => {
-        printOut(item[0] + " (#" + (index + 1) + "): " + item[1]);
-    });
+    if(curIndex == 1) {
+        return Math.floor(num1);
+    }
+    
+    if(curIndex == 2) {
+        return Math.ceil(num1);
+    }
+
+    if(curIndex == 3) {
+        return Math.pow(num1, num2);
+    }
+
+    if(curIndex == 4) {
+        return Math.sqrt(num1);
+    }
+
+    return 0;
+}
+
+const getMathType = (index) => {
+    if(index == 0) {
+        return mthRnd;
+    }
+
+    if(index == 1) {
+        return mthFlr;
+    }
+    
+    if(index == 2) {
+        return mthCl;
+    }
+
+    if(index == 3) {
+        return mthPow;
+    }
+
+    if(index == 4) {
+        return mthSqrt;
+    }
+
+    return "";
+}
+
+const results = [];
+for(let mathTypeIndex = 0; mathTypeIndex < 5; mathTypeIndex++) {
+    for(let i = 0; i < 3; i++) {
+        const num1 = Math.random() * 100;
+        let num2 = 0;
+
+        if(mathTypeIndex == 3) {
+            num2 = Math.max(2, roundRandom(10));
+        }
+
+        const mathResult = doTheMath(num1, num2, mathTypeIndex);
+        const result = [num1, num2, mathResult, mathTypeIndex];
+
+        results.push(result);
+    }
+}
+
+results.forEach((result) => {
+    printOut("#" + result[3] + " - " + getMathType(result[3])  + " - Value 1: " + result[0] + " - Value 2: " + result[1] + " - Result: " + result[2]);
 });
